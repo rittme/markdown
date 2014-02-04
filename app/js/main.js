f57e86b1a4d34f44a0222d0783377abb2a46038b
Bernardo Rittmeyer <bernardo@rittme.com>
2014-02-04 12:47:00 +0100

    Browser version

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,26 @@
-var file = require('file.js');
-var gui = require('nw.gui');
-
-var win = gui.Window.get();
-
-var editor;
-
-win.width = "862px";
-
-function clickInput(id) {
-  var event = document.createEvent('MouseEvents');
-  event.initMouseEvent('click');
-  document.getElementById(id).dispatchEvent(event);
-}
-
 document.addEventListener('keyup', function (e) {
-  if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
-    clickInput('open');
-  } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
-    clickInput('save');
-  } else if (e.keyCode == 27) { // escape
-    win.isFullscreen = !win.isFullscreen;
+  if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey && e.altKey) {
+    open();
+  } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey && e.altKey) {
+    save();
   }
 });
 
 
 function initPage() {
-
-	document.getElementById('open').addEventListener('change', function (e) {
-		console.log(this.value);
-		file.open(this.value, function (contents){
-			var tmp = document.createElement('div');
-			editor.innerText = contents;
-		});
-	});
-
-	document.getElementById('save').addEventListener('change', function (e) {
-		file.save(this.value, editor.innerHTML);
-	});
-
   editor = document.getElementById('editDiv');
   console.log("loaded");
   editor.focus();
+}
+
+function load() {
+  console.log("Content loaded");
+  if (localStorage.contents) {
+      editor.innerText = localStorage.contents;
+    }
+}
+
+function save() {
+  console.log("Content saved");
+  localStorage.contents = editor.innerHTML;
 }
